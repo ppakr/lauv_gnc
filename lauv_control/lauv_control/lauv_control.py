f23f12ac8b01cf580b0ffde32d4eed9fd8b6187a
ppakr <akira.techap@gmail.com>
2025-11-30 22:57:07 +0100

    feat: add initial LAUVControl class and .gitignore for cache files

---
+++ lauv_control/lauv_control/lauv_control.py
@@ -0,0 +1,30 @@
+# TODO: import lib
+
+from lauv_control.pid_controller import PIDController
+
+class LAUVControl:
+    def __init__(self):
+        # TODO: init node
+
+        # TODO: parameter setup
+
+        # TODO: pid set up
+
+        # TODO: ROS2 interfaces
+        pass
+
+
+    # TODO: callbacks
+    
+    def control_callback(self):
+    
+        pass
+
+    def odom_callback(self):
+
+        pass
+
+    # TODO: helper functions
+    
+    # TODO: utils
+
